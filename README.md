3ec668e0f97e9c0f208618b3098ed12c8327a448
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2024-06-18 17:45:41 -0400

    cleaned files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nypl-ocr
 Pipeline for running OCR on digitized catalog cards
+
+Run ```python3 benchmark.py```.
--- benchmark.py
+++ benchmark.py
@@ -2,37 +2,26 @@
 from doctr.io import DocumentFile
 from datetime import datetime
 from doctr.models import ocr_predictor
-import json
-import multiprocessing as mp
 from joblib import Parallel, delayed
-import os, sys
-
-NUM_CORES = 8
+import os
 
 model = ocr_predictor(det_arch="linknet_resnet18", pretrained=True)
-print(mp.current_process(),end='\n\n')
 
 def worker(file_name):
-    print(file_name)
+    print(f"{progress[file_name]}: {file_name}")
     doc = DocumentFile.from_pdf(file_name)
     return model(doc)
 
-def mp_handler(tasks):
-    p = mp.Pool(NUM_CORES)
-    p.map(worker, tasks)
-
-
-# if __name__ == "__main__":
-startTime = datetime.now()
+progress = {}
 tasks = []
 folder = "pdfs/split_pages/"
 for i, name in enumerate(os.listdir(folder)):
-    file_path = "original/split_pages/" + name
-    tasks.append(file_path)
-print("setup complete")
+    if "pdf" in name:
+        file_path = "pdfs/split_pages/" + name
+        tasks.append(file_path)
+        progress[file_path] = i
+print("setup complete",end='\n\n')
 
-#     mp_handler(tasks)
-
-
+startTime = datetime.now()
 results = Parallel(n_jobs=-1)(delayed(worker)(x) for x in tasks)
-print(datetime.now() - startTime)
+print("Benchmarked time: " + str(datetime.now() - startTime))
